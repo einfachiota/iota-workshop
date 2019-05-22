867e9fe089431b02d933283439abe43314a1710b
huhn <sehe89@gmail.com>
2019-05-22 18:50:19 +0200

    add german translation

--- code/4-send-tokens.js
+++ code/4-send-tokens.js
@@ -1,26 +1,27 @@
 ///////////////////////////////
-// Send tokens
+// IOTA Tokens versenden
 ///////////////////////////////
 
+// Wir importieren die IOTA JS Blibliothek und erstellen ein IOTA Objekt mit einem Devnet Provider.
 const iotaLibrary = require('@iota/core')
-
 const iota = iotaLibrary.composeAPI({
   provider: 'https://nodes.devnet.thetangle.org:443'
 })
 
-// Replace this with the seed you sent tokens too!
+// Ersetze dies durch den Seed, der zu der Adresse mit IOTA Tokens gehört.
 const seed =
   'HELLOWORLDHELLOWORLDHELLOWORLDHELLOWORLDHELLOWORLDHELLOWORLDHELLOWORLDHELLOWORL9D'
 
-// Create a wrapping function so we can use async/await
+// Erstelle  eine Wrapperfunktion, somit können  wir async und await verwenden.
 const main = async () => {
-  // Generate a different address from your seed
+  // Generiere eine andere neue Adresse aus dem Samen, hier bekommen wir eine Liste aus Addressen zurück. (Array)
   const receivingAddress = await iota.getNewAddress(seed, {
     index: 1,
     total: 1
   })
 
-  // Construct a TX to our new address
+  // Erstelle eine Transfer Objekt mit unsere der neuen Adresse. Wir holen die erste Adresse aus der Liste
+  // receivingAddress[0] gibt uns das erstes Element der Liste, also unsere neue Adresse.
   const transfers = [
     {
       value: 500,
@@ -28,17 +29,21 @@ const main = async () => {
       tag: 'MYMAGIC'
     }
   ]
-  console.log('Sending 500i to ' + receivingAddress)
+  console.log('Sende 500i zu ' + receivingAddress)
 
   try {
-    // Construct bundle and convert to trytes
+    // Hiermit erstellen wir unser Bundle Objekt im Trytes Format.
     const trytes = await iota.prepareTransfers(seed, transfers)
-    // Send bundle to node.
+
+    // Hiermit senden wir die Trytes zu dem IOTA Node.
     const response = await iota.sendTrytes(trytes, 3, 9)
 
-    console.log('Completed TXs')
+    
+    // Unsere Transaktion wurde erfolgreich gesendet. In der Kosnsole wird das Transaktionobjekt angezeigt.s
+    console.log('Senden beendet!')
     response.map(tx => console.log(tx))
   } catch (e) {
+    // Dieser Teil wird aufgerufen, falls oben etwas schief geht.
     console.log(e)
   }
 }
