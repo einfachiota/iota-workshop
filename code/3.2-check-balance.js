3ae0f918d139e78f08a8cbfbef169fe0708471b3
huhn <sehe89@gmail.com>
2019-05-22 18:41:30 +0200

    add german translation

--- code/3.2-check-balance.js
+++ code/3.2-check-balance.js
@@ -1,16 +1,18 @@
 ///////////////////////////////
-// Fetch balance of an address
+// Saldo einer Adresse holen
 ///////////////////////////////
 
+// Wir importieren die IOTA JS Blibliothek und erstellen ein IOTA Objekt mit einem Devnet Provider.
 const iotaLibrary = require('@iota/core')
-
 const iota = iotaLibrary.composeAPI({
   provider: 'https://nodes.devnet.thetangle.org:443'
 })
 
+// Wir rufen den Saldo dieser Addresse ab
 const address =
   'NBZLOBCWG9BAQTODDKNF9LYYTBOUWSQSGCWFQVZZR9QXCOAIBRYDTZOEGBGMZKJYZOPPGRGFFWTXUKUKW'
 
+// Mit Hilfe dieser funktion bekommen wir den aktuellen Saldo der Adresse. 
 iota
   .getBalances([address], 100)
   .then(({ balances }) => {
