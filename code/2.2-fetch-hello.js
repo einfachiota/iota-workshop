f76929946b48fe544ebb02b8718af827bff384ea
huhn <sehe89@gmail.com>
2019-05-22 18:37:33 +0200

    add german comments

--- code/2.2-fetch-hello.js
+++ code/2.2-fetch-hello.js
@@ -2,15 +2,17 @@
 // Fetch your HELLOWORLD Message
 ///////////////////////////////
 
+// Wir importieren die IOTA JS Blibliothek und erstellen ein IOTA Objekt mit einem Devnet Provider.
 const iotaLibrary = require('@iota/core')
-
 const iota = iotaLibrary.composeAPI({
   provider: 'https://nodes.devnet.thetangle.org:443'
 })
 
+// Dise Addresse wollen wir überprügen.
 const address =
   'HEQLOWORLDHELLOWORLDHELLOWORLDHELLOWORLDHELLOWORLDHELLOWORLDHELLOWORLDHELLOWOR99DMNFAQLWHD'
 
+// Hiermit bekommen wir alle Transaktionen, die an diese Adresse gesendet wurden.
 iota
   .findTransactionObjects({ addresses: [address] })
   .then(response => {
